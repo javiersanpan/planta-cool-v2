c45c5084b9534298e7e2cbd9a22e81604643581a
javiersanpan <javiersanpan@outlook.com>
2022-07-09 19:59:02 -0500

    Connected to new firebase project and tested database

--- ios-app/planta-cool-ios/ViewController.swift
+++ ios-app/planta-cool-ios/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAnalytics
 
 class ViewController: UIViewController {
+    
+    private let database = Database.database().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // FirebaseAnalytics event to check connection
+        Analytics.logEvent("InitScreen", parameters: ["message":"Firebase Integration successful"])
+        
+        // Database test
+        let object: [String: Any] = [
+            "regar": 0 as NSObject,
+            "humedad": "detectando el ",
+            "tanque": "detectando..."
+        ]
+        database.child("test").setValue(object)
     }
 
 
