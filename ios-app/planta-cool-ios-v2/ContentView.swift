845372c017fb4859fd0546a11dff561dbcd01efb
javiersanpan <javiersanpan@outlook.com>
2022-08-29 14:11:10 -0500

    Succesfully connected swift appa to firebase

--- ios-app/planta-cool-ios-v2/ContentView.swift
+++ ios-app/planta-cool-ios-v2/ContentView.swift
@@ -1,20 +1,27 @@
 import SwiftUI
+import FirebaseDatabase
 
 struct ContentView: View {
     
     var body: some View {
 
-        NavigationView{
-            VStack{
-            }
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text("YOUR PLANTS")
-                        .font(Font.headerTitle)
+        VStack {
+            
+            NavigationView {
+                VStack{}
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text("YOUR PLANTS")
+                            .font(Font.headerTitle)
+                    }
                 }
+                // This should be the plant ID
+                .navigationTitle("PLANT001")
+                
             }
+            
+            Label(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, systemImage: /*@START_MENU_TOKEN@*/"42.circle"/*@END_MENU_TOKEN@*/)
         }
-
     }
 }
 
